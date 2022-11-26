d00e99e38e5a6aff37a2694d831beffa5f028317
Mia Manabat <mmanabat@nd.edu>
2022-11-26 15:22:15 -0500

    inital commit

---
+++ CC01/streams.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <fstream>
+#include <string>
+
+#define IFSTREAM std::ifstream
+#define STRING std::string
+#define COUT std::cout
+#define ENDL std::endl
+
+
+
+int main(int argc, char** argv) {
+	
+
+	
+	return 0;
+}
